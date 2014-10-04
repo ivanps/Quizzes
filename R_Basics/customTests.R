b5c8d7c60dd94c373cb2444a3ed5c0b28b4c2f5d
ivanps <ivanpsmx@msn.com>
2014-10-04 00:52:52 -0500

    25 questions

--- R_Basics/customTests.R
+++ R_Basics/customTests.R
@@ -11,17 +11,29 @@
 # can be used for for the purpose, but it also re-evaluates the
 # expression which the user entered, so care must be taken.
 
-check_omnitest <- function(result) {
+check_omnitest <- function(correctExpr=NULL, correctVal=NULL, strict=FALSE) {
     # Get e
     e <- get('e', parent.frame())
-    rflag <- omnitest(result)
-    if (!rflag) {
-        qpts <<- .9*qpts
-        cat("Sorry, now question is worth ",qpts," points. \n")
+    if (!is.null(correctExpr)) {
+        rflag <- omnitest(correctExpr=correctExpr, strict=strict)
     } else {
-        cat("Got ",qpts," in this question.\n")
-        cal <<- cal + qpts
-        qpts <<- 10
+        rflag <- omnitest(correctVal=correctVal, strict=strict)
+    }
+
+    if (!is.null(e$skips)) {
+        swirl_out("***Test won't count because you skipped a question.***")
+        return(rflag)
+    }  
+    
+    if (!rflag) {
+        swirl_out("Sorry, now question is worth ",qpts*(.9)^(e$attempts-1)," points. \n")
+    } else {
+        swirl_out("Got ",qpts*(.9)^(e$attempts-2)," in this question.\n")
+        sid <<- eval(e$expr)
+        cal <<- cal + qpts*(.9)^(e$attempts-2)
+        if (e$row == NPREG+1) {
+            report_calif(round(cal, digits=2))
+        }     
     }
     rflag
 }
@@ -29,14 +41,36 @@ check_omnitest <- function(result) {
 check_var_is <- function(ans) {
     e <- get('e', parent.frame())
     rflag <- var_is_a('character','name')
+    
+    if (!is.null(e$skips)) {
+        swirl_out("***Test won't count because you skipped a question.***")
+        return(rflag)
+    }
+
     if (!rflag) {
-        qpts <<- .9*qpts
-        cat("Sorry, now question is worth ",qpts," points. \n")
+        swirl_out("Sorry, now question is worth ",qpts*(.9)^(e$attempts-1)," points. \n")
     } else {
-        cat("Got ",qpts," in this question.\n")
+        swirl_out("Got ",qpts*(.9)^(e$attempts-2)," in this question.\n")
         sid <<- eval(e$expr)
-        cal <<- cal + qpts
-        qpts <<- 10
+        cal <<- cal + qpts*(.9)^(e$attempts-2)      
+        if (e$row == NPREG+1) {
+            report_calif(round(cal, digits=2))
+        }     
     }
     rflag
+}
+
+report_calif <- function(calificacion) {
+    u <- as.integer(runif(1, min=0, max=100))
+    cat("\n\n\nTEST RESULTS \n")
+    cat("Grade: ", calificacion, "\n")
+    cat("Coded grade: ", base64(paste(name,calificacion,u,sep="-"))[[1]], "\n\n")
+    readline("...")
+    cat("If you want to report your grade, open your browser and go \n")
+    cat("to the following link and report to grade. \n")
+    cat("https://docs.google.com/a/itesm.mx/forms/d/1CTLG2eVqOUjurzsb5GxOefb5gXvXTa1twMK8HYgFENY/viewform")
+    cat("\n\n\n")
+    readline("...")
+    cat("Don't forget that you can take the test again, but you have\n")
+    cat("to send your grade at the end, before the deadline.\n\n\n")
 }
--- R_Basics/initLesson.R
+++ R_Basics/initLesson.R
@@ -3,8 +3,25 @@
 # the user's working directory and thus be accessible to them
 # throughout the lesson.
 
+rm(list=ls())
 suppressMessages(library(RCurl))
 
+NPREG <- 25
 cal <- 0
-qpts <- 10
 sid <- ""
+qpts <- 100/NPREG
+
+# Path to installed lesson
+.lessonpath <- file.path(path.package("swirl"), "Courses", "Quizzes",
+                         "R_Basics")
+# Path to dataset
+.datapath <- file.path(.lessonpath, "flag.data.txt")
+# Load dataset
+flags <- read.csv(.datapath, header=FALSE)
+# Set column names
+colnames(flags) <- c("name", "landmass", "zone", "area", "population",
+                     "language", "religion", "bars", "stripes", "colours",
+                     "red", "green", "blue", "gold", "white", "black",
+                     "orange", "mainhue", "circles", "crosses", "saltires",
+                     "quarters", "sunstars", "crescent", "triangle",
+                     "icon", "animate", "text", "topleft", "botright")
